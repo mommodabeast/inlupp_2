fa08779769e7507b898796cd3e1256e91d397b7f
Muhammad Saad <musa3667@trygger.it.uu.se>
2023-10-31 14:57:42 +0100

    Mommos utils filer

--- utils.c
+++ utils.c
@@ -1,106 +1,187 @@
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
-#include "utils.h"
 
 bool not_empty(char *str)
 {
   return strlen(str) > 0;
 }
 
-answer_t ask_question(char *question, check_func check, convert_func convert) {
-bool a = false;
-int buff_size = 255;
-char buff[buff_size];
-while(a == false) {
-    printf("%s", question);
-    read_string(buff, buff_size);
-    if(check(buff)) {
-        a = true;
-    } else {
+bool is_number(char *str)
+{
+int i = 0;
 
-    }
-}
-return convert(buff);
+if (str[0] == '-')
+{
+if (str[1] == '\0')
+return false;
+//i = 1;
 }
 
-bool is_number(char *str) {
-int length = strlen(str);
-for(int i = 0; i < length; i++) {
-    if(isdigit(str[i])) {
-        if(i == length-1) {
-            return true;
-        } else {
-          
-        }
-    } 
-    else if(str[i] == '-' && i == 0){
-        
-    } else {
-        return false;
-    }
-}
+for (; str[i] != '\0'; i++)
+{
+if (!isdigit(str[i]))
+{
 return false;
 }
+}
+
+return true;
+}
+
+char print(char *string) {
+
+  char ord;
+
+    for (int i = 0; i <= strlen(string); i++)
+    {
+      ord = putchar(string[i]); 
+    }
+
+return ord;
+}
+
+char printin(char *string) {
+
+    char ord = print(string);
+
+    return printf("%c\n", ord);
+
+}
+
+void clear_input_buffer()
+{
+int c;
+do
+{
+  c = getchar();
+}
+while (c != '\n' && c != EOF);
+}
+
+/*int ask_question_int(char *question)
+{ 
+  int result = 0;
+  int conversions = 0;
+  do
+    {
+      printf("%s\n", question);
+      conversions = scanf("%d", &result);
+      clear_input_buffer();
+      putchar('\n');
+    }
+  while (conversions < 1);
+  return result;
+}*/
 
 int ask_question_int(char *question)
 {
-  answer_t answer = ask_question(question, is_number, (convert_func) atoi);
-  return answer.int_value; 
+  answer_t answer = ask_question(question, is_number, (convert_function) atoi);
+  return answer.int_value; // svaret som ett heltal
 }
 
-int read_string(char *buf, int buf_siz) {
-    char character = getchar();
-    int counter = 0;
-    while (character != '\n' && counter <= buf_siz-1) {
-        buf[counter] = character;
-        counter++;
-        character = getchar();
+int read_string(char *buf, int bufsize) {
+    
+  scanf("%[^\n]", buf);
+  
+  int counter = 0;
+  int length = strlen(buf);
+
+  if (length < bufsize - 1)
+  {
+    for(int i = 0; i < length; i++)
+      {
+        counter = counter + 1;
+      }
+  } else {
+    for (int j = 0; j < bufsize; j++)
+    {
+      counter = counter + 1;
     }
-    buf[counter] = '\0';
-    return counter;
+  }
+
+  clear_input_buffer();
+  buf[counter] = '\0';
+  return counter;
 }
+
+/*char *ask_question_string(char *question, char *buf, int buf_siz)
+
+{
+
+char name[buf_siz];
+
+  do
+    {
+      printf("%s\n", question);
+      scanf("%s", name);
+      
+    }
+  while (read_string(buf, buf_siz) != 0);
+
+  return strdup(name);
+}*/
 
 char *ask_question_string(char *question)
 {
-  return ask_question(question, not_empty, (convert_func) strdup).string_value;
+  return ask_question(question, not_empty, (convert_function) strdup).string_value;
 }
 
-void print(char *str) {
-  int length = strlen(str);
-  for(int i = 0; i < length; i++) {
-    putchar(str[i]);
+answer_t ask_question(char *question, check_function check, convert_function convert) {
+
+char answer[256];
+//char question[256];
+
+while (1) {
+
+  printf("%s ", question);
+  read_string(answer, sizeof(answer));
+
+  /*int input_length = read_string(answer, sizeof(answer));*/
+
+  if (check(answer)) {
+    answer_t convertedAnswer = convert(answer);
+    return convertedAnswer;
+  
+  } else {
+    printf("Invalid answer format. Try again\n");
   }
+
 }
 
-void println(char *str) {
-  print(str);
-  putchar('\n');
+
 }
+
+
 
 bool is_shelf(char *str) {
-int length = strlen(str);
-for(int i = 0; i < length; i++) {
-    if(isdigit(str[i]) && i != 0) {
-        if(i == length-1) {
-            return true;
-        } else {
-          
+    int len = strlen(str);
+    bool is_shlf = true;
+    
+    // ASCII value check if first letter is a capital letter and stringlength is more than one
+    if (str[0] >= 65 && str[0] <= 90 && len > 1) {
+        
+        for (int i = 1; i < len; i = i + 1) {
+            
+            if (!isdigit(str[i])) {
+                is_shlf = false;
+                break;
+            
+            } else { }
         }
-    } 
-    else if(isalpha(str[i]) && !isdigit(str[i]) && i == 0){
         
     } else {
-        return false;
+        is_shlf = false;
     }
-}
-return false;
+    
+    return is_shlf;
 }
 
 char *ask_question_shelf(char *question)
 {
-  return ask_question(question, is_shelf, (convert_func) strdup).string_value;
+  answer_t answer = ask_question(question, is_shelf, (convert_function) strdup);
+  return answer.string_value; // svaret som ett heltal
 }
--- utils.h
+++ utils.h
@@ -3,24 +3,30 @@
 #include <stdbool.h>
 
 extern char *strdup(const char *);
+
 typedef union { 
   int   int_value;
   float float_value;
   char *string_value;
 } answer_t;
+typedef bool(*check_function)(char *answer);
+typedef answer_t(*convert_function)(char *answer);
 
-typedef bool(*check_func)(char *);
-typedef answer_t(*convert_func)(char*);
 
-bool not_empty(char *str);
-answer_t ask_question(char *question, check_func check, convert_func convert);
-char *ask_question_string(char *question);
+bool is_number(char *str);
 int read_string(char *buf, int buf_siz);
+bool not_empty(char *str);
 bool is_number(char *str);
 int ask_question_int(char *question);
-void print(char *str);
-void println(char *str);
-bool is_shelf(char *str);
+//int ask_question_int(char *question);
+//char *ask_question_string(char *question, char *buf, int buf_siz);
+void clear_input_buffer();
+char print(char *string);
+char printin(char *string);
+answer_t ask_question(char *question, check_function check, convert_function convert);
+char *ask_question_string(char *question);
+
+bool is_shelf(char *string);
 char *ask_question_shelf(char *question);
 
 #endif 
